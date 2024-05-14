9b7b6ee00769e01c7803ad75c97113e4fc54b150
NealD00 <120283275+NealD00@users.noreply.github.com>
2024-05-14 14:10:54 -0600

    primer comnit

---
+++ database/migrations/2024_05_14_181604_participaciones.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('participaciones', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('participante_id')->constrained()->onDelete('cascade');
+            $table->foreignId('evento_id')->constrained()->onDelete('cascade');
+            $table->dateTime('fecha_hora');
+            $table->integer('puntos')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('participaciones');
+    }
+};
